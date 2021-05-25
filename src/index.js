2494193ddf0994e1523a2d62ab21ae6a5380d655
Gareth Field <field.gareth@gmail.com>
2021-05-25 06:53:02 -0400

    yay!

--- src/index.js
+++ src/index.js
@@ -2,25 +2,112 @@ const { Command, flags } = require('@oclif/command');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const figlet = require('figlet');
+const format = require('date-fns/format');
 
+const FONT = 'Doh';
 const CUSTOM = 'CUSTOM';
 const WORK = 'WORK';
+const WORK_MESSAGE = 'Allons-y!';
 const RECHARGE = 'RECHARGE';
+const RECHARGE_MESSAGE = 'Recharge!';
+
+const MESSAGE_OPTIONS = {
+	font: FONT,
+	horizontalLayout: 'Default',
+	verticalLayout: 'Default',
+};
+
+const numOrCustom = val => {
+	const p = parseInt(val.split(' ')[0], 10);
+	if (p === NaN) {
+		return CUSTOM;
+	}
+	return p;
+};
+
+const questions = [
+	{
+		name: 'workTime',
+		type: 'list',
+		message: 'Primary block length?',
+		// choices: ['25 min', '30 min', '40 min', '45 min', 'Custom value'],
+		choices: ['25 min', '30 min', '40 min', '45 min'],
+		filter: val => {
+			return numOrCustom(val);
+		},
+	},
+	{
+		name: 'rechargeTime',
+		type: 'list',
+		message: 'Recharge time?',
+		// choices: ['5 min', '10 min', '15 min', '20 min', 'Custom value'],
+		choices: ['5 min', '10 min', '15 min', '20 min'],
+		filter: val => {
+			return numOrCustom(val);
+		},
+	},
+];
+
+const ms = min => min * 60000;
 
 class PomCommand extends Command {
+	printMessage(type) {
+		const message = type === WORK ? WORK_MESSAGE : RECHARGE_MESSAGE;
+		if (type === WORK) {
+			this.log(chalk.green(figlet.textSync(message, MESSAGE_OPTIONS)));
+		} else if (type === RECHARGE) {
+			this.log(chalk.blue(figlet.textSync(message, MESSAGE_OPTIONS)));
+		}
+		this.log(format(new Date(), 'EEEE MMM do, h:mm bbb'));
+		/*
+		 * EEEE = Monday
+		 *	MMM = Jan
+		 * do = 1st
+		 * h = 1
+		 * mm = 08
+		 * bbb = am (inc noon & midnight)
+		 */
+	}
+	go(workTime, rechargeTime) {
+		// if (workTime === NaN || rechargeTime === NaN) {
+		// 	this.error('Not implemented yet!');
+		// }
+		this.log(
+			`Work ${workTime} minutes, rest ${rechargeTime} minutes!\nCtrl + Shift + C to close! (On my machine! ~Gareth)`
+		);
+		const totalTime = ms(workTime) + ms(rechargeTime);
+		this.printMessage(WORK);
+		setInterval(() => {
+			this.printMessage(WORK);
+		}, totalTime);
+		const setrechargeTimer = () => {
+			setInterval(() => {
+				this.printMessage(RECHARGE);
+			}, totalTime);
+		};
+		setTimeout(() => {
+			this.printMessage(RECHARGE);
+			setrechargeTimer();
+		}, ms(workTime));
+	}
 	async run() {
-		const { flags } = this.parse(PomCommand);
-		const name = flags.name || 'world';
+		// const { flags } = this.parse(PomCommand);
+		// const name = flags.name || 'world';
 		// if arg1, find rest block length, or back out
 		// if arg1 and arg2, go go go
 		// else find work and recharge block lengths
-		this.log(`hello ${name} from ./src/index.js`);
+		// this.log(`hello ${name} from ./src/index.js`);
+		this.log(chalk.red.bold('Pom: Pomodoro Timer!'));
+		const { workTime, rechargeTime } = await inquirer.prompt(questions); // this can come back out to a fn for MESSAGE_OPTIONS
+		this.go(workTime, rechargeTime);
 	}
 }
 
-PomCommand.description = `Describe the command here
+PomCommand.description = `Pomodoro timer CLI!
 ...
-Extra documentation goes here
+Fire this baby up with \`pom\` and pick a
+work time and recharge time! Customization
+coming soon!
 `;
 
 PomCommand.flags = {
@@ -28,84 +115,7 @@ PomCommand.flags = {
 	version: flags.version({ char: 'v' }),
 	// add --help flag to show CLI version
 	help: flags.help({ char: 'h' }),
-	name: flags.string({ char: 'n', description: 'name to print' }),
+	// name: flags.string({ char: 'n', description: 'name to print' }),
 };
 
 module.exports = PomCommand;
-
-const init = () => {
-	console.log(chalk.red.bold('Pom: Pomodoro Timer!'));
-};
-
-const numOrCustom = val => {
-	const p = parseInt(val.split(' ')[0], 10);
-	p === NaN ? CUSTOM : p;
-};
-
-const askQuestions = () => {
-	const questions = [
-		{
-			name: 'workTime',
-			type: 'list',
-			message: 'Primary block length?',
-			choices: ['25 min', '30 min', '40 min', '45 min', 'Custom value'],
-			filter: val => {
-				return numOrCustom(val);
-			},
-		},
-		{
-			name: 'rechargeTime',
-			type: 'list',
-			message: 'Recharge time?',
-			choices: ['5 min', '10 min', '15 min', '20 min', 'Custom value'],
-			filter: val => {
-				return numOrCustom(val);
-			},
-		},
-	];
-	return inquirer.prompt(questions);
-};
-
-const printMessage = type => {
-	const message = type === WORK ? 'Allonz-y!' : 'Recharge!';
-	const options = {
-		font: 'Doh',
-		horizontalLayout: 'Default',
-		verticalLayout: 'Default',
-	};
-	if (type === WORK) {
-		console.log(chalk.green(figlet.textSync(message, options)));
-	} else if (type === RECHARGE) {
-		console.log(chalk.blue(figlet.textSync(message, options)));
-	}
-	console.log(new Date());
-};
-
-const toMill = min => min * 60000;
-
-const startTimer = (workTime, restTime) => {
-	console.log(`Work ${workTime} minutes, rest ${restTime} minutes!`);
-	const totalTime = toMill(workTime) + toMill(restTime);
-	printMessage(WORK);
-	var workTimer = setInterval(() => {
-		printMessage(WORK);
-	}, totalTime);
-	const setRestTimer = () => {
-		var restTimer = setInterval(() => {
-			printMessage(RECHARGE);
-		}, totalTime);
-	};
-	setTimeout(() => {
-		printMessage(RECHARGE);
-		setRestTimer();
-	}, toMill(workTime));
-};
-
-const run = async () => {
-	init();
-	const answers = await askQuestions();
-	const { workTime, restTime } = answers;
-	startTimer(workTime, restTime);
-};
-
-run();
